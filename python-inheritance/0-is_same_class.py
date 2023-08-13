1ea23026fca7288b2928959b8fd6298d429cd9cb
sadiqsaaima <saaimadole@gmail.com>
2023-08-13 00:11:42 -0700

    soul

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
-"""
-    is_kind_of-class
+"""object is exactly an instance of the specified class
 """
 
 
-def is_kind_of_class(obj, a_class):
-    """
-    function that returns True if the object is an instance of
-    or if the object is an instance of a class that inherited from,
-    the specified class ; otherwise False.
-    """
-    return isinstance(obj, a_class)
+def is_same_class(obj, a_class):
+    """ `considering of  class an object"""
+    if type(obj) is a_class:
+        return True
+    return False
